8d723e61834f240b28956378da9543586bc6b3c0
Ciprian.Talmacel <cprn_talmacel@yahoo.com>
2016-03-29 11:32:20 +0300

    added preliminary testsController

---
+++ swarms/FetchResult.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ciprian on 3/18/16.
+ */
+var fetchResult = {
+    start:function (result) {
+        this.result = result;
+        this.home("gotResult");
+    }
+}
+
+fetchResult;
