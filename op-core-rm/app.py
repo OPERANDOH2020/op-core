1ebeea51f0be996d5b0c189dea38edce61840d10
Constantinos Patsakis <kpatsak@gmail.com>
2016-07-02 01:24:57 +0300

    Now it's POST

--- op-core-rm/app.py
+++ op-core-rm/app.py
@@ -15,15 +15,55 @@ __hdr = {"Content-Type": "application/json", "Accept": "*/*", }
 __URL = "http://snf-706921.vm.okeanos.grnet.gr:8080/dan/operando/pdr/access_node/getResults"
 __URL_TGT = "http://snf-706921.vm.okeanos.grnet.gr:8080/authentication/aapi/tickets/"
 URL_TGT__2GET = 'https://snf-706921.vm.okeanos.grnet.gr:8443/dan'
+__URL_PC='http://server02tecnalia.westeurope.cloudapp.azure.com:8095/operando/core/pc/osp_policy_evaluator?user_id=pjgrace&osp_id=Hospital+OSP'
 app = Flask(__name__)
 TGT=""
 
-@app.route('/Results', methods=['GET'])
-def results_get(ticket=None, serviceID=None, paramValues=None):
+
+
+# SQL_PatientsOfDoctor=SELECT p.FirstName, p.LastName, p.Weight FROM Patient p WHERE p.IDPrimaryCaregiver = ? 
+# SQL_PatientWithID=SELECT p.Id, p.FirstName, p.LastName, p.Weight FROM Patient p WHERE p.ID = ?
+@app.route('/Results', methods=['POST'])
+def results_get():
     # read the request parameters
-    tckt = request.args.get('ticket')
-    sID = request.args.get('ServiceID')
-    params = request.args.get('ParamValues')
+    data = request.get_json(force=True)
+    try:
+"""
+{
+                “OspId”: “FoodCoach” ,
+“RoleId”: “1”,
+“QueryId”: “FoodCoach;2”,
+                “UserIds”: [“36”]
+}
+"""
+        tckt = data['ticket']
+        sID = data['ServiceID']
+        params = data['ParamValues']
+    except (KeyError, TypeError, ValueError):
+        raise JsonError(description='Invalid value.')
+    print 10*"-"+tckt+"||"+sID+"||"+params
+
+    logdata={}    
+    logdata["requesterType"] = "MODULE"
+    #TODO: Fix the requesterID
+    logdata["requesterId"] =sID
+    logdata["logPriority"] ="LOW"
+    logdata[ "logDataType"] ="INFO"
+    logdata["title"] ="Request to RM"
+    logdata[ "description"]= "access to query with params: %s" % params
+    logdata[ "keywords"]=  ["query"] 
+    logdata=json.dumps(logdata)
+    log_resp=requests.post('http://server02tecnalia.westeurope.cloudapp.azure.com:8090/operando/core/ldb/log', headers=__hdr, data=logdata)
+    #test the response
+    logmsg=log_resp.text
+    try:
+        msg=json.loads(logmsg)
+        if msg["type"].lower()!="ok":
+            print "error logging"
+    except:
+        print "error logging"
+
+
     # validate the received ticket
     URL_validate = "https://snf-706921.vm.okeanos.grnet.gr:8443/casv415/serviceValidate?ticket=%s&service=https://snf-706921.vm.okeanos.grnet.gr:8443/dan" % tckt
 
@@ -41,14 +81,33 @@ def results_get(ticket=None, serviceID=None, paramValues=None):
         # check whether the request has a valid ticket
         if elmt is not None:
             if elmt.tag.split("}")[1] == "user" and elmt.text == sID:
-                # get a ticket for DAN
-                tckt4DAN = requests.post(__URL_TGT+TGT, data=URL_TGT__2GET).text
-                params=json.loads(params)
-                params["st"]=tckt4DAN
-                params=json.dumps(params)
-                # execute the query to DAN                
-                req = urllib2.Request(__URL, headers=__hdr, data=params)
-                return Response(urllib2.urlopen(req).read(), status=200, mimetype='application/json')
+
+                #determine if the query has to be executed
+                dt={}
+                dt["requester_id"]="Hospital OSP"
+                dt["subject"]="Doctor"
+                dt["requested_url"]="Weight"
+                dt["action"]="Access"
+                dt["attributes"]= []
+                dt=[dt]
+                data=json.dumps(dt)
+                req = requests.post(__URL_PC, headers=__hdr, data=data)
+                resp= req.text
+                print resp
+                resp=json.loads(resp)
+                if resp["status"].lower()=="true":
+                    # get a ticket for DAN
+                    tckt4DAN = requests.post(__URL_TGT+TGT, data=URL_TGT__2GET).text
+                    params=json.loads(params)
+                    params["st"]=tckt4DAN
+                    params=json.dumps(params)
+                    # execute the query to DAN                
+                    req = urllib2.Request(__URL, headers=__hdr, data=params)
+                    return Response(urllib2.urlopen(req).read(), status=200, mimetype='application/json')
+                else:
+                    resp = jsonify({'status': 401, 'message': resp["compliance"], })
+                    resp.status_code = 404
+                    return resp
             else:
                 resp = jsonify({'status': 401, 'message': 'Invalid ticket', })
                 resp.status_code = 404
@@ -72,5 +131,4 @@ if __name__ == '__main__':
     data = json.dumps(data)
     req = urllib2.Request('http://snf-706921.vm.okeanos.grnet.gr:8080/authentication/aapi/tickets', headers=__hdr, data=data)
     TGT = urllib2.urlopen(req).read()  
-    print TGT  
-    app.run(host='0.0.0.0',port=8080, threaded=True, debug=False)
+    app.run(host='0.0.0.0',port=8080, threaded=True, debug=True)
