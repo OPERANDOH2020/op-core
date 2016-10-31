3498946a13818d017afb76d548a8d82037e775b4
cp <cp@localhost.localdomain>
2016-10-31 13:08:24 +0200

    New RM capabilities supporting OData integration

--- op-core-rm/app.py
+++ op-core-rm/app.py
@@ -9,127 +9,191 @@ import requests
 import json
 import ConfigParser
 
+# load credentials
 config = ConfigParser.RawConfigParser()
-
+config.read('config.cfg')
+user = config.get('Credentials', 'user')
+passwd = config.get('Credentials', 'password')
 __hdr = {"Content-Type": "application/json", "Accept": "*/*", }
-__URL = "http://snf-706921.vm.okeanos.grnet.gr:8080/dan/operando/pdr/access_node/getResults"
-__URL_TGT = "http://snf-706921.vm.okeanos.grnet.gr:8080/authentication/aapi/tickets/"
-URL_TGT__2GET = 'https://snf-706921.vm.okeanos.grnet.gr:8443/dan'
-__URL_PC='http://server02tecnalia.westeurope.cloudapp.azure.com:8095/operando/core/pc/osp_policy_evaluator?user_id=pjgrace&osp_id=Hospital+OSP'
-__URL_LOGDB='http://server02tecnalia.westeurope.cloudapp.azure.com:8090/operando/core/ldb/log'
+
+# URLs
+__DAN_url = 'https://snf-706921.vm.okeanos.grnet.gr:8443/op-pdr-dan/%s'
+__aapi_st_url = 'https://snf-706921.vm.okeanos.grnet.gr:8443/authentication/aapi/tickets/%s'
+__aapi_tgt_url = 'https://snf-706921.vm.okeanos.grnet.gr:8443/authentication/aapi/tickets'
+__aapi_tckt_val_url = 'https://snf-706921.vm.okeanos.grnet.gr:8443/authentication/aapi/tickets/%s/validate?serviceId=%s'
+__URL_LOGDB = 'http://server02tecnalia.westeurope.cloudapp.azure.com:8090/operando/core/ldb/log'
+
 app = Flask(__name__)
-TGT=""
+# TODO
+# contact PC and apply prefs
+
 
 def Log2CAS():
-	config.read('config.cfg')
-	user = config.get('Credentials', 'user')
-	passwd = config.get('Credentials', 'password')
-	data={"password": passwd,"username": user}
-	data = json.dumps(data)
-	req = urllib2.Request('http://snf-706921.vm.okeanos.grnet.gr:8080/authentication/aapi/tickets', headers=__hdr, data=data)
-	return urllib2.urlopen(req).read()  
+    data = '{"password": "%s","username": "%s"}' % (user, passwd)
+    response = requests.post(__aapi_tgt_url, data=data,
+                             verify=False, headers=__hdr)
+    return response.text
 
-def GetTicketForDAN(tgt):
-	ticket_resp=requests.post(__URL_TGT+tgt, data=URL_TGT__2GET)
-	#check if the ticket has expired and 
-	if ticket_resp.text=="TicketGrantingTicket could not be found":
-		#get a fresh TGT
-		TGT=Log2CAS()	
-		#get a new service ticket	
-		ticket_resp=requests.post(__URL_TGT+TGT, data=URL_TGT__2GET)
-	return ticket_resp.text	
 
-def ValidateReceivedTicket(tckt,sID):
-    URL_validate = "https://snf-706921.vm.okeanos.grnet.gr:8443/casv415/serviceValidate?ticket=%s&service=https://snf-706921.vm.okeanos.grnet.gr:8443/dan" % tckt
-    # since this is a self signed ticket we have to disable certificate
-    ctx = ssl.create_default_context()
-    ctx.check_hostname = False
-    ctx.verify_mode = ssl.CERT_NONE
-    # verification
-    val = urllib2.urlopen(URL_validate, context=ctx).read()
+def joinSTR(lst):
+    return ','.join(str(e) for e in lst)
+
+
+def GetST():
+    # get service ticket for dan
+    response = requests.post(__aapi_st_url %
+                             TGT, data='op-pdr/dan', verify=False)
+    return response.text
+
+
+def ValidateReceivedTicket(tckt, sID):
+    r = requests.get(__aapi_tckt_val_url %
+                     (tckt, '/operando/rm/'), verify=False)
+    return r.status_code == 200
+    # the rest of it not valid till we get the user mappings
     try:
         # validate the XML
-        root = ET.fromstring(val)
+        root = ET.fromstring(r.text)
         namespaces = {'cas': 'http://www.yale.edu/tp/cas'}
         elmt = root.find('*/*')
         # check whether the request has a valid ticket
         if elmt is not None:
-            if elmt.tag.split("}")[1] == "user" and elmt.text == sID:
+            if elmt.tag.split("}")[1] == "user":  # and elmt.text == sID:
                 return True
     except:
-        return False    
+        return False
     return False
 
-@app.route('/Results', methods=['POST'])
-def results_get():
-    # read the request parameters
-    data = request.get_json(force=True)
-    try:
-        tckt = data['ticket']
-        sID = data['ServiceID']
-        params = data['ParamValues']
-        roleID= data['RoleID']
-    except (KeyError, TypeError, ValueError):
-        raise JsonError(description='Invalid value.')
 
-    logdata={}    
+def logdata(requesterId, action, actiontype):
+    logdata = {}
     logdata["requesterType"] = "MODULE"
-    #TODO: Fix the requesterID
-    logdata["requesterId"] =sID
-    logdata["logPriority"] ="LOW"
-    logdata[ "logDataType"] ="INFO"
-    logdata["title"] ="Request to RM"
-    logdata[ "description"]= "access to query with params: %s" % params
-    logdata[ "keywords"]=  ["query"] 
-    logdata=json.dumps(logdata)
+    # TODO: Fix the requesterID
+    logdata["requesterId"] = requesterId
+    logdata["logPriority"] = "LOW"
+    logdata["logDataType"] = "INFO"
+    logdata["title"] = actiontype
+    logdata["description"] = action
+    logdata["keywords"] = ["query"]
+    logdata = json.dumps(logdata)
 
-    log_resp=requests.post(__URL_LOGDB, headers=__hdr, data=logdata)
-    #test the response
-    logmsg=log_resp.text
+    log_resp = requests.post(__URL_LOGDB, headers=__hdr, data=logdata)
+    # test the response
+    logmsg = log_resp.text
     try:
-        msg=json.loads(logmsg)
-        if msg["type"].lower()!="ok":
+        msg = json.loads(logmsg)
+        if msg["type"].lower() != "ok":
             print "error logging"
     except:
         print "error logging"
 
-    # validate the received ticket
-    # added 1==1 to bypass ticket validation, but it's working
-    if ValidateReceivedTicket(tckt,sID)==True or 1==1:
-        #determine if the query has to be executed
-        dt={}
-        dt["subject"]=roleID
-        #for both queries these properties are the same
-        dt["requester_id"]="Hospital OSP"
-        dt["requested_url"]="Weight"
-        dt["action"]="Access"
-        dt["attributes"]= []
-        dt=[dt]
-        data=json.dumps(dt)
-        req = requests.post(__URL_PC, headers=__hdr, data=data)
-        resp= req.text
-        
-        resp=json.loads(resp)
-        if resp["status"].lower()=="true":
-            # get a ticket for DAN
-            tckt4DAN = GetTicketForDAN(TGT)
-            params=json.loads(params)
-            params["st"]=tckt4DAN
-            params=json.dumps(params)
-            # execute the query to DAN                
-            req = requests.post(__URL, headers=__hdr, data=params)
-            return Response(req.text, status=200, mimetype='application/json')
-        else:
-            resp = jsonify({'status': 401, 'message': resp["compliance"], })
-            resp.status_code = 404
-            return resp
-    else:
-        resp = jsonify({'status': 401, 'message': 'Invalid ticket', })
+
+@app.route('/', defaults={'path': ''}, methods=['POST', 'GET', 'PUT'])
+@app.route('/<path:path>', methods=['POST', 'GET', 'PUT'])
+def catch_all(path):
+    if path[:7] != "Results":
+        resp = jsonify({'status': 401, 'message': 'Invalid address', })
         resp.status_code = 404
         return resp
+    else:
+        addr = path[8:]
+    if request.method == 'POST':
+        # this is an insert
+                # validate the received ticket
+        receivedST = request.headers["service-ticket"]
+        if not ValidateReceivedTicket(receivedST, 'operando/rm/'):
+            return Response("Service ticket (ST) is invalid", status=400, mimetype='application/json')
+        ST = GetST()
+        headers = {'service-ticket': ST, 'Content-Type': 'application/json',
+                   'Accept': '*/*', 'osp-identifier': 'built-in', 'psp-user-identifier': 'unknown'}
+        # have to check with PC whether this guy can insert.
+        # ITI API pending
+        # suppose that the response is true
+        r = requests.post(__DAN_url % addr, headers=headers,
+                          data=request.data, verify=False)
+        if r.status_code == 200:
+            ks = json.loads(request.data)
+            logdata("requesterId", "insert into table %s" % addr,
+                    "fieds:%s||status:%s" % (joinSTR(ks.keys()), "Granted"))
+            return Response(r.text, status=200, mimetype='application/json')
+        else:
+            returnResponse(r.text,
+                           status=r.status_code, mimetype='application/json')
+    elif request.method == 'GET':
+        # this is a select
+        # validate the received ticket
+        receivedST = request.headers["service-ticket"]
+        if not ValidateReceivedTicket(receivedST, 'operando/rm/'):
+            return Response("Service ticket (ST) is invalid", status=400, mimetype='application/json')
+        ST = GetST()
+        headers = {'service-ticket': ST, 'Accept': '*/*',
+                   'osp-identifier': 'built-in', 'psp-user-identifier': 'unknown', }
+        redirect = "?" + request.query_string if request.query_string else ""
+        r = requests.get(__DAN_url % (addr + redirect),
+                         headers=headers, verify=False)
+        # check whether the response is OK
+        if r.status_code == 200:
+            jsonResponse = json.loads(r.text)
+            if "results" in jsonResponse['d'].keys():  # many ids
+                data2respond = jsonResponse['d']['results']
+                for d in data2respond:
+                    ks = d.keys()
+                    ks.remove("__metadata")
+                    ks.remove("MetadatavalueDetails")
+                    ks[ks.index("Iduser")] = d['Iduser']
+                    # request ks from PC
+                    # pending changes from ITI
+                    # suppose that the response is true
 
-# "TicketGrantingTicket could not be found"
+                logdata("requesterId", "select from table %s" %
+                        path, "fieds:%s||status:%s" % (joinSTR(ks), "Granted"))
+                return Response(r.text, status=200, mimetype='application/json')
+            else:
+                data2respond = jsonResponse['d']
+                ks = data2respond.keys()
+                ks.remove("__metadata")
+                ks.remove("MetadatavalueDetails")
+                ks[ks.index("Iduser")] = data2respond['Iduser']
+                # request ks from PC
+                # pending changes from ITI
+                # suppose that the response is true
+                logdata("requesterId", "select from table %s" %
+                        path, "fieds:%s||status:%s" % (joinSTR(ks), "Granted"))
+                return Response(r.text, status=200, mimetype='application/json')
+
+        else:
+            returnResponse(r.text,
+                           status=r.status_code, mimetype='application/json')
+    elif request.method == 'PUT':
+        # this is an update
+        # validate the received ticket
+        receivedST = request.headers["service-ticket"]
+        if not ValidateReceivedTicket(receivedST, 'operando/rm/'):
+            return Response("Service ticket (ST) is invalid", status=400, mimetype='application/json')
+        ST = GetST()
+        headers = {'service-ticket': ST, 'Content-Type': 'application/json',
+                   'Accept': '*/*', 'osp-identifier': 'built-in', 'psp-user-identifier': 'unknown', }
+        # have to check with PC whether this guy can insert.
+        # ITI API pending
+        # suppose that the response is true
+        r = requests.put(__DAN_url % addr, headers=headers,
+                         data=request.data, verify=False)
+        if r.status_code == 200:
+            ks = json.loads(request.data)
+            logdata("requesterId", "update table %s" % addr,
+                    "fieds:%s||status:%s" % (joinSTR(ks.keys()), "Granted"))
+            return Response("updated", status=200, mimetype='application/json')
+        else:
+            returnResponse(r.text,
+                           status=r.status_code, mimetype='application/json')
+        return Response("updated", status=200, mimetype='application/json')
+    else:
+        return "Method not supported"
+
+
 if __name__ == '__main__':
-	TGT=Log2CAS()
-	#start the service
-	app.run(host='0.0.0.0',port=8080, threaded=True, debug=False)
+    TGT = Log2CAS()
+    ST = GetST()
+    # start the service
+    app.run(host='0.0.0.0',port=8080, threaded=True, debug=False)
+    print ST
