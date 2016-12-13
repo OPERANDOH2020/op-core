c4d74e954a0614dcbdd3341c68b27ffa5b71d7ed
gechevarria <gorka.echevarria@tecnalia.com>
2016-12-13 12:52:48 +0100

    Included some more records in the database creation script so the data is kept after a new deployment

--- op-core-logdb/database/scripts/creation.sql
+++ op-core-logdb/database/scripts/creation.sql
@@ -28,7 +28,7 @@ CREATE TABLE `LOGS` (
   `LOGGER` varchar(50) DEFAULT NULL,
   `LEVEL` varchar(10) DEFAULT NULL,
   `REQUESTERTYPE` varchar(20) DEFAULT NULL,
-  `REQUESTERID` varchar(20) DEFAULT NULL,
+  `REQUESTERID` varchar(50) DEFAULT NULL,
   `LOGPRIORITY` varchar(10) DEFAULT NULL,
   `KEYWORDS` varchar(50) DEFAULT NULL,
   `TITLE` varchar(50) DEFAULT NULL,
@@ -42,6 +42,20 @@ CREATE TABLE `LOGS` (
 
 LOCK TABLES `LOGS` WRITE;
 /*!40000 ALTER TABLE `LOGS` DISABLE KEYS */;
-INSERT INTO `LOGS` VALUES ('username','2016-06-07 15:10:22,174','io.swagger.api.impl.LogApiServiceImpl','INFO','Module','1001','Low','[keyword1, keyword2, keyword3]','First log','First log for testing purposes');
+INSERT INTO `LOGS` (`USER_ID`, `DATED`, `LOGGER`, `LEVEL`, `REQUESTERTYPE`, `REQUESTERID`, `LOGPRIORITY`, `KEYWORDS`, `TITLE`, `MESSAGE`) VALUES
+('001', '2016-12-09 09:10:22,174', 'io.swagger.api.impl.WdTaskUserPrivacySettingsCheck', 'INFO', 'MODULE', '1001', 'NORMAL', '[privacy]', 'Privacy settings discrepancy', 'The privacy settings for user 001 with OSP 12 are not as required. This requires action.');
+
+
+INSERT INTO `LOGS` (`USER_ID`, `DATED`, `LOGGER`, `LEVEL`, `REQUESTERTYPE`, `REQUESTERID`, `LOGPRIORITY`, `KEYWORDS`, `TITLE`, `MESSAGE`) VALUES
+('002', '2016-12-10 10:11:21,075', 'io.swagger.api.impl.UsersApiServiceImpl', 'ERROR', 'MODULE', '1002', 'NORMAL', '[users,delete]', 'Delete UPP', 'Error. No document exits to be deleted.');
+
+INSERT INTO `LOGS` (`USER_ID`, `DATED`, `LOGGER`, `LEVEL`, `REQUESTERTYPE`, `REQUESTERID`, `LOGPRIORITY`, `KEYWORDS`, `TITLE`, `MESSAGE`) VALUES
+('002', '2016-12-11 10:11:21,075', 'io.swagger.api.impl.OSPApiServiceImpl', 'INFO', 'PROCESS', '1003', 'NORMAL', '[osp,privacy]', 'OSP PUT', 'OSP PUT received.');
+INSERT INTO `LOGS` (`USER_ID`, `DATED`, `LOGGER`, `LEVEL`, `REQUESTERTYPE`, `REQUESTERID`, `LOGPRIORITY`, `KEYWORDS`, `TITLE`, `MESSAGE`) VALUES
+('003', '2016-12-12 09:10:22,174', 'io.swagger.api.impl.OffersApiServiceImpl', 'INFO', 'MODULE', '1004', 'NORMAL', '[offer,update]', 'Update Offer', 'The offer has been updated succesfully.');
+
+
+INSERT INTO `LOGS` (`USER_ID`, `DATED`, `LOGGER`, `LEVEL`, `REQUESTERTYPE`, `REQUESTERID`, `LOGPRIORITY`, `KEYWORDS`, `TITLE`, `MESSAGE`) VALUES
+('004', '2016-12-13 10:11:21,075', 'io.swagger.api.impl.PersonaldataApiServiceImpl', 'INFO', 'MODULE', '1004', 'NORMAL', '[annonymization,personal]', 'Anonymized Personal Data', 'The personal data has been anonymized.');
 /*!40000 ALTER TABLE `LOGS` ENABLE KEYS */;
 UNLOCK TABLES;
