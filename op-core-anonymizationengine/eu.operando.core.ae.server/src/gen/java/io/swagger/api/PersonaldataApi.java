2e08c5a69fe7e46b41cd79e49a5f9bac9895b0df
gechevarria <gorka.echevarria@tecnalia.com>
2016-12-21 16:09:45 +0100

    Added a change in order to get some annonymized data for T3 test

--- op-core-anonymizationengine/eu.operando.core.ae.server/src/gen/java/io/swagger/api/PersonaldataApi.java
+++ op-core-anonymizationengine/eu.operando.core.ae.server/src/gen/java/io/swagger/api/PersonaldataApi.java
@@ -16,23 +16,20 @@ import io.swagger.model.InlineResponse2003;
 
 @Path("/personaldata")
 
-@Produces({ "application/json" })
 @io.swagger.annotations.Api(description = "the personaldata API")
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-05-10T09:36:15.284Z")
 public class PersonaldataApi  {
    private final PersonaldataApiService delegate = PersonaldataApiServiceFactory.getPersonaldataApi();
 
    @GET
-   @Path("/core/ae/personaldata/{requester_id}/search")
-   @Consumes({ "application/json" })
-   @Produces({ "application/json" })
+   @Path("/core/ae/personaldata/{requester_id}/search")   
    @io.swagger.annotations.ApiOperation(value = "Gets individual or colective personal data by receiving as parameter a query especifiyng the data wanted to be recovered and the requester id.", notes = "Gets individual or colective personal data by receiving as parameter a query especifiyng the data wanted to be recovered and the requester id.", response = InlineResponse2003.class, tags={ "DataUnits", })
    @io.swagger.annotations.ApiResponses(value = { 
        @io.swagger.annotations.ApiResponse(code = 200, message = "Successful operation.", response = InlineResponse2003.class),
        
        @io.swagger.annotations.ApiResponse(code = 200, message = "Unexpected error", response = InlineResponse2003.class) })
    public Response getPersonalData(@ApiParam(value = "The requester identifier number",required=true) @PathParam("requester_id") String requesterId
-,@ApiParam(value = "query encapsulating the set of data units and the personal ids of the people whose personal data is wanted to be received.") @QueryParam("query") String query
+,@ApiParam(value = "query encapsulating the set of data units and the personal ids of the people whose personal data is wanted to be received."), @QueryParam("query") String query
 ,@Context SecurityContext securityContext)
    throws NotFoundException {
        return delegate.getPersonalData(requesterId,query,securityContext);
