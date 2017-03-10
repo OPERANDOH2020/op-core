b5954d055839b92c42120e9803482a78504d7d57
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-10 17:37:02 +0100

    first test

---
+++ op-int/eu.operando.int.src.dvp.core.ldb.server.test00/src/main/docker/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysql -u $MYSQL_DB_USER -p$MYSQL_DB_PASSWORD -h $MYSQL_DB_HOST -D $MYSQL_DB_NAME -e sql/creation.sql
--- op-int/eu.operando.int.src.dvp.core.ldb.server/src/main/docker/sql/creation.sql
+++ op-int/eu.operando.int.src.dvp.core.ldb.server/src/main/docker/sql/creation.sql
@@ -35,26 +35,3 @@ CREATE TABLE `LOGS` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `LOGS`
---
-
-LOCK TABLES `LOGS` WRITE;
-/*!40000 ALTER TABLE `LOGS` DISABLE KEYS */;
-INSERT INTO `LOGS` (`USER_ID`, `DATED`, `LOGGER`, `LEVEL`, `REQUESTERTYPE`, `REQUESTERID`, `LOGPRIORITY`, `KEYWORDS`, `TITLE`, `MESSAGE`) VALUES
-('001', '2016-12-09 09:10:22,174', 'io.swagger.api.impl.WdTaskUserPrivacySettingsCheck', 'INFO', 'MODULE', '1001', 'NORMAL', '[privacy]', 'Privacy settings discrepancy', 'The privacy settings for user 001 with OSP 12 are not as required. This requires action.');
-
-
-INSERT INTO `LOGS` (`USER_ID`, `DATED`, `LOGGER`, `LEVEL`, `REQUESTERTYPE`, `REQUESTERID`, `LOGPRIORITY`, `KEYWORDS`, `TITLE`, `MESSAGE`) VALUES
-('002', '2016-12-10 10:11:21,075', 'io.swagger.api.impl.UsersApiServiceImpl', 'ERROR', 'MODULE', '1002', 'NORMAL', '[users,delete]', 'Delete UPP', 'Error. No document exits to be deleted.');
-
-INSERT INTO `LOGS` (`USER_ID`, `DATED`, `LOGGER`, `LEVEL`, `REQUESTERTYPE`, `REQUESTERID`, `LOGPRIORITY`, `KEYWORDS`, `TITLE`, `MESSAGE`) VALUES
-('002', '2016-12-11 10:11:21,075', 'io.swagger.api.impl.OSPApiServiceImpl', 'INFO', 'PROCESS', '1003', 'NORMAL', '[osp,privacy]', 'OSP PUT', 'OSP PUT received.');
-INSERT INTO `LOGS` (`USER_ID`, `DATED`, `LOGGER`, `LEVEL`, `REQUESTERTYPE`, `REQUESTERID`, `LOGPRIORITY`, `KEYWORDS`, `TITLE`, `MESSAGE`) VALUES
-('003', '2016-12-12 09:10:22,174', 'io.swagger.api.impl.OffersApiServiceImpl', 'INFO', 'MODULE', '1004', 'NORMAL', '[offer,update]', 'Update Offer', 'The offer has been updated succesfully.');
-
-
-INSERT INTO `LOGS` (`USER_ID`, `DATED`, `LOGGER`, `LEVEL`, `REQUESTERTYPE`, `REQUESTERID`, `LOGPRIORITY`, `KEYWORDS`, `TITLE`, `MESSAGE`) VALUES
-('004', '2016-12-13 10:11:21,075', 'io.swagger.api.impl.PersonaldataApiServiceImpl', 'INFO', 'MODULE', '1004', 'NORMAL', '[annonymization,personal]', 'Anonymized Personal Data', 'The personal data has been anonymized.');
-/*!40000 ALTER TABLE `LOGS` ENABLE KEYS */;
-UNLOCK TABLES;
