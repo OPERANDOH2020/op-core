ccb81bac53e17033eb4a0aa9dff17f93c03349b7
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-10 17:44:28 +0100

    name of test fixed

--- op-int/eu.operando.int.src.dvp.core.ldb.server.test00/src/main/docker/run.sh
+++ op-int/eu.operando.int.src.dvp.core.ldb.server.test00/src/main/docker/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysql -u $MYSQL_DB_USER -p$MYSQL_DB_PASSWORD -h $MYSQL_DB_HOST -D $MYSQL_DB_NAME -e sql/creation.sql
+mysql -u $MYSQL_DB_USER -p$MYSQL_DB_PASSWORD -h $MYSQL_DB_HOST -D $MYSQL_DB_NAME -e sql/test.sql
