1f3773f56488e49abb6a05bb5fa0c905df3e2351
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-10 18:00:40 +0100

    test log docker in jenkins ...

--- op-int/eu.operando.int.src.dvp.core.ldb.server.test00/src/main/docker/run.sh
+++ op-int/eu.operando.int.src.dvp.core.ldb.server.test00/src/main/docker/run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 mysql -u $MYSQL_DB_USER -p$MYSQL_DB_PASSWORD -h $MYSQL_DB_HOST -D $MYSQL_DB_NAME -e sql/test.sql
+echo "el perro the san roque no tiene rabo"
+echo "se de buen tinta que ramon rodriguez se lo ha cortado"
+exit 3
