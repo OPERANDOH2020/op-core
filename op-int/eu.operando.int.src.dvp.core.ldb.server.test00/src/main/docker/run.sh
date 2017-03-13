c61f6e1dc5bc154a938b9e9a99fb7d84eff1ac82
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-13 09:05:36 +0100

    fixes message

--- op-int/eu.operando.int.src.dvp.core.ldb.server.test00/src/main/docker/run.sh
+++ op-int/eu.operando.int.src.dvp.core.ldb.server.test00/src/main/docker/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-mysql -u $MYSQL_DB_USER -p$MYSQL_DB_PASSWORD -h $MYSQL_DB_HOST -D $MYSQL_DB_NAME -e sql/test.sql
-echo "el perro the san roque no tiene rabo"
-echo "se de buen tinta que ramon rodriguez se lo ha cortado"
-echo "Fin"
-exit 3
+mysql -u $MYSQL_DB_USER -p$MYSQL_DB_PASSWORD -h $MYSQL_DB_HOST -D $MYSQL_DB_NAME < sql/test.sql
+if [ $? -eq 0 ]
+then
+  echo "ALL SUCCESSFUL"
+else
+  echo "ERROR"
+fi
