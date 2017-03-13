85a8d586c072ed4dc67f3ffa0979696d0f27d3ea
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-13 10:42:35 +0100

    fixes extension

--- op-int/eu.operando.int.src.dvp.core.ldb.server.test01/src/main/docker/run.sh
+++ op-int/eu.operando.int.src.dvp.core.ldb.server.test01/src/main/docker/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -jar eu.operando.int.src.dvp.core.ldb.server.test01-ALPHA
+java -jar eu.operando.int.src.dvp.core.ldb.server.test01-ALPHA.jar
 if [ $? -eq 0 ]
 then
   echo "ALL SUCCESSFUL"
