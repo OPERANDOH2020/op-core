9006befe6dd351b43990aa03aed45bff0ad87e65
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-21 16:49:01 +0100

    pdb server test00 image created

--- op-int/eu.operando.int.core.pdb.server.test00.src.dvp/src/main/docker/run.sh
+++ op-int/eu.operando.int.core.pdb.server.test00.src.dvp/src/main/docker/run.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
-mysql -u $MYSQL_DB_USER -p$MYSQL_DB_PASSWORD -h $MYSQL_DB_HOST -D $MYSQL_DB_NAME < sql/test.sql
+
+all_Successful=0
+
+curl -d @mongo/osp_hospital.json -H "Content-Type:application/json" http://integration.operando.dmz.lab.esilab.org:8096/operando/core/pdb/OSP
 if [ $? -eq 0 ]
+then
+  echo "OSP HOSPITAL SUCCESSFULLY LOADED"
+else
+  echo "ERROR"
+  all_Successful=1
+fi
+
+curl -d @mongo/osp_POST.json -H "Content-Type:application/json" http://integration.operando.dmz.lab.esilab.org:8096/operando/core/pdb/OSP
+if [ $? -eq 0 ]
+then
+  echo "OSP POST SUCCESSFULLY LOADED"
+else
+  echo "ERROR"
+  all_Successful=1
+fi
+
+curl -d @mongo/osp_foodcoach.json -H "Content-Type:application/json" http://integration.operando.dmz.lab.esilab.org:8096/operando/core/pdb/OSP
+if [ $? -eq 0 ]
+then
+  echo "OSP FOODCOACH SUCCESSFULLY LOADED"
+else
+  echo "ERROR"
+  all_Successful=1
+fi
+
+curl -H "Content-Type: application/json" -X POST -d "{\"user_id\": \"pjgrace\",\"user_preferences\": [],\"subscribed_osp_policies\": [{\"osp_id\": \"Hospital OSP\",\"access_policies\": [{\"subject\": \"Doctor\",\"permission\": true,\"action\": \"Access\",\"resource\": \"Medical Information\",\"attributes\": []}]},{\"osp_id\": \"Food Coach demo\",\"access_policies\": [{\"subject\": \"Dietician\",\"permission\": true,\"action\": \"Access\", \"resource\": \"Medical Information\", \"attributes\": []}]}], \"subscribed_osp_settings\": []}" http://integration.operando.dmz.lab.esilab.org:8096/operando/core/pdb/user_privacy_policy 
+if [ $? -eq 0 ]
+then
+  echo "PJGRACE SUCCESSFULLY LOADED"
+else
+  echo "ERROR"
+  all_Successful=1
+fi
+
+if [ $all_Successful -eq 0 ]
 then
   echo "ALL SUCCESSFUL"
 else
