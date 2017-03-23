b0f10a2fd5cc6cbddce6f3438464e805b2569d3b
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-23 08:31:18 +0100

    puts OLD_VARS as ENV variables for the docker

--- op-int/eu.operando.int.core.rm.server.src.dvp/src/main/docker/run.sh
+++ op-int/eu.operando.int.core.rm.server.src.dvp/src/main/docker/run.sh
@@ -15,26 +15,26 @@ echo RM Url Path lenght: $RM_URLPATH_LENGHT
 sed -i "s|path\[8:\]|\path\[$RM_URLPATH_LENGHT:\]|g"  /sources/app.py
 
 #__DAN_url = 'https://snf-706921.vm.okeanos.grnet.gr:8443/op-pdr-dan/%s'
-OLD_DAN_ENDPOINT=https://snf-706921.vm.okeanos.grnet.gr:8443/op-pdr-dan
+#OLD_DAN_ENDPOINT=https://snf-706921.vm.okeanos.grnet.gr:8443/op-pdr-dan
 sed -i -e "s|$OLD_DAN_ENDPOINT|$DAN_ENDPOINT|" \
 /sources/app.py
 
 #__aapi_st_url = 'https://snf-706921.vm.okeanos.grnet.gr:8443/authentication/aapi/tickets/%s'
 #__aapi_tgt_url = 'https://snf-706921.vm.okeanos.grnet.gr:8443/authentication/aapi/tickets'
 #__aapi_tckt_val_url = 'https://snf-706921.vm.okeanos.grnet.gr:8443/authentication/aapi/tickets/%s/validate?serviceId=%s'
-OLD_AAPI_ENDPOINT=https://snf-706921.vm.okeanos.grnet.gr:8443/authentication
+#OLD_AAPI_ENDPOINT=https://snf-706921.vm.okeanos.grnet.gr:8443/authentication
 sed -i -e "s|$OLD_AAPI_ENDPOINT|$AAPI_ENDPOINT|" \
 /sources/app.py
 
 #__URL_LOGDB = 'http://server02tecnalia.westeurope.cloudapp.azure.com:8090/operando/core/ldb/log'
-OLD_LDB_ENDPOINT=http://server02tecnalia.westeurope.cloudapp.azure.com:8090/operando/core/ldb
+#OLD_LDB_ENDPOINT=http://server02tecnalia.westeurope.cloudapp.azure.com:8090/operando/core/ldb
 sed -i -e "s|$OLD_LDB_ENDPOINT|$LDB_ENDPOINT|" \
 /sources/app.py
 
-OLD_AAPI_USER=username
+#OLD_AAPI_USER=username
 sed -i -e "s|$OLD_AAPI_USER$|$AAPI_USER|" \
 /sources/config.cfg
-OLD_AAPI_PASSWORD=password
+#OLD_AAPI_PASSWORD=password
 sed -i -e "s|$OLD_AAPI_PASSWORD$|$AAPI_PASSWORD|" \
 /sources/config.cfg
 
