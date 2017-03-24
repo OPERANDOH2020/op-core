4c764a2167afabe9f3de389060e416180f8321e9
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-24 12:09:07 +0100

    adds number

--- op-int/eu.operando.int.core.rm.server.src.dvp/src/main/docker/run.sh
+++ op-int/eu.operando.int.core.rm.server.src.dvp/src/main/docker/run.sh
@@ -9,10 +9,10 @@ echo RM Url Path lenght: $RM_URLPATH_LENGHT
 
 # http://askubuntu.com/questions/76808/how-do-i-use-variables-in-a-sed-command
 # I replace the endpoint path
-sed -i "s|path\[:7\] != \"Results\"|\path\[:$RM_URLPATH_LENGHT\+8\] != \"$RM_URLPATH\/Results\"|g"  /sources/app.py
+sed -i "s|path\[:7\] != \"Results\"|\path\[:$(($RM_URLPATH_LENGHT + 8))\] != \"$RM_URLPATH\/Results\"|g"  /sources/app.py
 sed -i "s|path == \"monitor\"|\path == \"$RM_URLPATH\/monitor\"|g"  /sources/app.py
 
-sed -i "s|path\[8:\]|\path\[$RM_URLPATH_LENGHT\+9:\]|g"  /sources/app.py
+sed -i "s|path\[8:\]|\path\[$(($RM_URLPATH_LENGHT + 9)):\]|g"  /sources/app.py
 
 #__DAN_url = 'https://snf-706921.vm.okeanos.grnet.gr:8443/op-pdr-dan/%s'
 #OLD_DAN_ENDPOINT=https://snf-706921.vm.okeanos.grnet.gr:8443/op-pdr-dan
