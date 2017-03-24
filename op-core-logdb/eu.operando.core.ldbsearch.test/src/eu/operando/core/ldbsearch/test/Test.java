220d59796800c94858a4f7f9f193c8f2a3666524
gechevarria <gorka.echevarria@tecnalia.com>
2017-03-24 10:30:23 +0100

    Small fix in LogDBSearch

--- op-core-logdb/eu.operando.core.ldbsearch.test/src/eu/operando/core/ldbsearch/test/Test.java
+++ op-core-logdb/eu.operando.core.ldbsearch.test/src/eu/operando/core/ldbsearch/test/Test.java
@@ -48,7 +48,7 @@ public class Test {
 	    keyWords.add("keyword1");
 	    String jsonKeyWords = new Gson().toJson(keyWords);
 	    List<Pair> queryParams = new ArrayList<Pair>();
-	    queryParams.add(new Pair ("dateFrom","2016-07-04 08:50:25"));
+	    queryParams.add(new Pair ("dateFrom","2017-03-22 08:50:25"));
 	    queryParams.add(new Pair ("dateTo","2017-03-30 08:50:25"));
 	    queryParams.add(new Pair ("logLevel",""));
 	    queryParams.add(new Pair ("requesterType",""));
@@ -56,9 +56,9 @@ public class Test {
 	    queryParams.add(new Pair ("website_id",""));
 	    queryParams.add(new Pair ("logPriority",""));
 	    queryParams.add(new Pair ("title",""));
-	    //queryParams.add(new Pair ("logType",LogTypeEnum.NOTIFICATION.toString()));
+	    queryParams.add(new Pair ("logType",""));
 	    queryParams.add(new Pair ("keyWords",jsonKeyWords));
-	    //queryParams.add(new Pair ("affectedUserId","28"));
+	    queryParams.add(new Pair ("affectedUserId",""));
 	    
 	    Map<String, String> headerParams = new HashMap<String, String>(); 
 	    Map<String, Object> formParams = new HashMap<String, Object>(); 
