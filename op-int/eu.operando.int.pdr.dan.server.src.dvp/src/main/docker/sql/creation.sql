5d1c145d9b0e07ec6fa20ac33340e3403e83ec0b
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-24 20:22:38 +0100

    only add structure

--- op-int/eu.operando.int.pdr.dan.server.src.dvp/src/main/docker/sql/creation.sql
+++ op-int/eu.operando.int.pdr.dan.server.src.dvp/src/main/docker/sql/creation.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.5.49, for debian-linux-gnu (x86_64)
+--CREATE DATABASE  IF NOT EXISTS `dan` /*!40100 DEFAULT CHARACTER SET latin1 */;
+--USE `dan`;
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
--- Host: localhost    Database: operando_logdb
+-- Host: snf-706921.vm.okeanos.grnet.gr    Database: dan
 -- ------------------------------------------------------
--- Server version	5.5.49-0ubuntu0.14.04.1
+-- Server version	5.5.52-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,27 +18,72 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `LOGS`
+-- Table structure for table `METADATAFIELDREGISTRY`
 --
-DROP TABLE IF EXISTS `LOGS`;
+
+DROP TABLE IF EXISTS `METADATAFIELDREGISTRY`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `LOGS` (
-  `USER_ID` varchar(20) DEFAULT NULL,
-  `DATED` varchar(50) DEFAULT NULL,
-  `LOGGER` varchar(50) DEFAULT NULL,
-  `LEVEL` varchar(10) DEFAULT NULL,
-  `REQUESTERTYPE` varchar(20) DEFAULT NULL,
-  `REQUESTERID` varchar(20) DEFAULT NULL,
-  `LOGPRIORITY` varchar(10) DEFAULT NULL,
-  `KEYWORDS` varchar(50) DEFAULT NULL,
-  `TITLE` varchar(50) DEFAULT NULL,
-  `MESSAGE` varchar(1000) DEFAULT NULL,
-  `LOGTYPE` varchar(20) DEFAULT NULL,
-  `AFFECTED_USER_ID` varchar(20) DEFAULT NULL
+CREATE TABLE `METADATAFIELDREGISTRY` (
+  `id` int(11) NOT NULL,
+  `element` varchar(255) NOT NULL,
+  `label` varchar(45) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+--
+-- Table structure for table `METADATAVALUES`
+--
+
+DROP TABLE IF EXISTS `METADATAVALUES`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `METADATAVALUES` (
+  `id` int(11) NOT NULL,
+  `metadatafield_id` int(11) DEFAULT NULL,
+  `text_value` varchar(255) DEFAULT NULL,
+  `lang` varchar(45) DEFAULT '*',
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_fkey_idx` (`user_id`),
+  KEY `metadata_field_fkey_idx` (`metadatafield_id`),
+  CONSTRAINT `metadata_field_fkey` FOREIGN KEY (`metadatafield_id`) REFERENCES `METADATAFIELDREGISTRY` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user_fkey` FOREIGN KEY (`user_id`) REFERENCES `USER` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `QUERY`
+--
+
+DROP TABLE IF EXISTS `QUERY`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `QUERY` (
+  `idquery` int(11) NOT NULL,
+  `query` varchar(255) NOT NULL,
+  PRIMARY KEY (`idquery`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `USER`
+--
+
+DROP TABLE IF EXISTS `USER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `USER` (
+  `iduser` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  PRIMARY KEY (`iduser`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40000 ALTER TABLE `USER` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -47,5 +94,4 @@ CREATE TABLE `LOGS` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-03-23 11:53:44
-
+-- Dump completed on 2016-10-14 12:55:32
