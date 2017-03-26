f34941c1b0a6deffea00323ebf3a91d14a55a8d8
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-26 11:54:12 +0200

    checks the time effect

--- op-int/eu.operando.int.core.pdb.server.test.01.src.dvp/src/main/docker/run.sh
+++ op-int/eu.operando.int.core.pdb.server.test.01.src.dvp/src/main/docker/run.sh
@@ -4,11 +4,14 @@ all_Successful=0
 
 cd /sources
 python3 -m unittest
+
+sleep 10
+
 if [ $? -eq 0 ]
 then
   echo "Final Result:"
   echo "Errorcode: $?"
-  echo "ALL SUCCESSFUL"
+  echo "ALL_SUCCESSFUL"
 else
   echo "Final Result:"
   echo "Errorcode: $?"
