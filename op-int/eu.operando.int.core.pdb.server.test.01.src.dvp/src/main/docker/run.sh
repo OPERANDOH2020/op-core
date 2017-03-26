a281bf21f11b4646723993b7dab3d247ee00ae01
Gorka Benguria <gorka.benguria@tecnalia.com>
2017-03-26 11:29:40 +0200

    more logs

--- op-int/eu.operando.int.core.pdb.server.test.01.src.dvp/src/main/docker/run.sh
+++ op-int/eu.operando.int.core.pdb.server.test.01.src.dvp/src/main/docker/run.sh
@@ -6,7 +6,11 @@ cd /sources
 python3 -m unittest
 if [ $? -eq 0 ]
 then
+  echo "Final Result:"
+  echo "Errorcode: $?"
   echo "ALL SUCCESSFUL"
 else
+  echo "Final Result:"
+  echo "Errorcode: $?"
   echo "ERROR"
 fi
